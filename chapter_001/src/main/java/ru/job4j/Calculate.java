b6cd87854877e332eda36d23e9b5f2587c0d4938
IvanBelyaev <gh0st84@mail.ru>
2017-06-15 20:51:43 +0300

    task 4123

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 /**
- * Calculate
+ * Calculate.
  * @author Ivan Belyaev
  * @since 18.05.2017
  * @version 1.0
@@ -12,6 +12,6 @@ public class Calculate {
 	 * @param args - command line arguments
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello world");	
+		System.out.println("Hello world");
 	}
 }
