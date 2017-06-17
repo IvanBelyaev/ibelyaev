4ccb5cafda55884454dd62635eb4eb32438a9774
IvanBelyaev <gh0st84@mail.ru>
2017-06-17 23:22:11 +0300

    task 188

---
+++ chapter_001/src/main/java/ru/job4j/condition/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Package for position of point task.
+ *
+ * @author Ivan Belyaev (gh0st84@mail.ru)
+ * @version $Id$
+ * @since 1.0
+ */
---
+++ chapter_001/src/test/java/ru/job4j/condition/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Package for position of point test.
+ *
+ * @author Ivan Belyaev (gh0st84@mail.ru)
+ * @version $Id$
+ * @since 1.0
+ */
