beb912f8a3e3cb553ad2068195ba37487afeead2
IvanBelyaev <gh0st84@mail.ru>
2017-07-05 18:04:34 +0300

    Проверка что одно слово находиться в другом слове [#228]

--- chapter_001/src/test/java/ru/job4j/exam/SubstringSearchTest.java
+++ chapter_001/src/test/java/ru/job4j/exam/SubstringSearchTest.java
@@ -13,13 +13,24 @@ import static org.junit.Assert.assertThat;
  */
 public class SubstringSearchTest {
 	/**
-	 * Test for the contains method.
+	 * Test for the contains method, when the substring is contained in string.
 	 */
 	@Test
 	public void whenContainsTheStringHasThisSubstringThenTrue() {
 		SubstringSearch substringSearch = new SubstringSearch();
 		boolean methodReturns = substringSearch.contains("Привет", "иве");
 		boolean expected = true;
+		assertThat(methodReturns, is(expected));
+	}
+
+	/**
+	 * Test for the contains method, when the substring is not contained in the string.
+	 */
+	@Test
+	public void whenContainsTheStringDoesNotHaveThisSubstringThenFalse() {
+		SubstringSearch substringSearch = new SubstringSearch();
+		boolean methodReturns = substringSearch.contains("Привет", "игла");
+		boolean expected = false;
 		assertThat(methodReturns, is(expected));
 	}
 }
