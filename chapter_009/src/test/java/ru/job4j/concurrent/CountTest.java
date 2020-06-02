7af2ee6bac3f8cdfd324a21d7dcf65d06f57d498
IvanBelyaev <gh0st84@mail.ru>
2020-06-02 21:00:04 +0300

    2. JCIP. Настройка библиотеки [#283651]

---
+++ chapter_009/src/test/java/ru/job4j/concurrent/CountTest.java
@@ -0,0 +1,60 @@
+package ru.job4j.concurrent;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * CountTest.
+ *
+ * @author Ivan Belyaev
+ * @version 1.0
+ * @since 02.06.2020
+ */
+public class CountTest {
+
+    /**
+     * Класс описывает нить со счетчиком.
+     */
+    private class ThreadCount extends Thread {
+        /** Counter. */
+        private final Count count;
+
+        /**
+         * Constructor.
+         * @param count counter.
+         */
+        private ThreadCount(final Count count) {
+            this.count = count;
+        }
+
+        /**
+         * Increments the counter by one.
+         */
+        @Override
+        public void run() {
+            this.count.increment();
+        }
+    }
+
+    /**
+     * Simple multithreaded class checking.
+     * @throws InterruptedException exceptions.
+     */
+    @Test
+    public void whenExecute2ThreadThen2() throws InterruptedException {
+        //Создаем счетчик.
+        final Count count = new Count();
+        //Создаем нити.
+        Thread first = new ThreadCount(count);
+        Thread second = new ThreadCount(count);
+        //Запускаем нити.
+        first.start();
+        second.start();
+        //Заставляем главную нить дождаться выполнения наших нитей.
+        first.join();
+        second.join();
+        //Проверяем результат.
+        assertThat(count.get(), is(2));
+    }
+}
---
+++ chapter_009/src/test/java/ru/job4j/concurrent/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Tests for multithreading classes.
+ *
+ * @author Ivan Belyaev (gh0st84@mail.ru)
+ * @version $Id$
+ * @since 1.0
+ */
+package ru.job4j.concurrent;
