556bd1b2e54caad9e71cd035d8d8bdbc2a472b7c
IvanBelyaev <gh0st84@mail.ru>
2020-12-02 21:01:29 +0300

    1. Реализовать шаблон Producer Consumer. [#283634]

---
+++ chapter_009/src/test/java/ru/job4j/structures/SimpleBlockingQueueTest.java
@@ -0,0 +1,46 @@
+package ru.job4j.structures;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * SimpleBlockingQueueTest.
+ *
+ * @author Ivan Belyaev
+ * @version 1.0
+ * @since 02.12.2020
+ */
+public class SimpleBlockingQueueTest {
+    /**
+     * Test for SimpleBlockingQueue.
+     * @throws InterruptedException - possible exception.
+     */
+    @Test
+    public void whenThreeElementsOffersThenThreeElementsPolls() throws InterruptedException {
+        SimpleBlockingQueue<Integer> sbq = new SimpleBlockingQueue<>(2);
+        List<Integer> result = new ArrayList<>();
+        Thread producer = new Thread(() -> {
+            for (int i = 0; i < 3; i++) {
+                sbq.offer(i);
+            }
+        });
+        Thread consumer = new Thread(() -> {
+            for (int i = 0; i < 3; i++) {
+                result.add(sbq.poll());
+            }
+        });
+
+        producer.start();
+        consumer.start();
+        producer.join();
+        consumer.join();
+
+        assertThat(result, is(Arrays.asList(0, 1, 2)));
+    }
+}
